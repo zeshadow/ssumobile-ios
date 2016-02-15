4e6d3cfeb9b14313650705546de6dddfaf3f7ddb
Eric Amorde <eric.amorde@gmail.com>
2016-02-15 19:59:53 +0900

    Add detailed logging for debug builds

--- SSUMobile/Core/SSULogging.h
+++ SSUMobile/Core/SSULogging.h
@@ -20,10 +20,20 @@ static const DDLogLevel ddLogLevel = DDLogLevelError;
 
 + (void) setupLogging;
 
-#define SSULogError(frmt, ...)   DDLogError(frmt, ##__VA_ARGS__)
-#define SSULogWarn(frmt, ...)    DDLogWarn(frmt, ##__VA_ARGS__)
-#define SSULogInfo(frmt, ...)    DDLogInfo(frmt, ##__VA_ARGS__)
-#define SSULogDebug(frmt, ...)   DDLogDebug(frmt, ##__VA_ARGS__)
-#define SSULogVerbose(frmt, ...) DDLogVerbose(frmt, ##__VA_ARGS__)
+// http://stackoverflow.com/a/26364055
+#ifdef DEBUG
+#define SSULogError(fmt, ...)       DDLogError((@"%s [%d] \t" fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SSULogWarn(fmt, ...)        DDLogWarn((@"%s [%d] \t" fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SSULogInfo(fmt, ...)        DDLogInfo((@"%s [%d] \t" fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SSULogDebug(fmt, ...)       DDLogDebug((@"%s [%d] \t" fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define SSULogVerbose(fmt, ...)     DDLogVerbose((@"%s [%d] \t" fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#define SSULogError(frmt, ...)      DDLogError(frmt, ##__VA_ARGS__)
+#define SSULogWarn(frmt, ...)       DDLogWarn(frmt, ##__VA_ARGS__)
+#define SSULogInfo(frmt, ...)       DDLogInfo(frmt, ##__VA_ARGS__)
+#define SSULogDebug(frmt, ...)      DDLogDebug(frmt, ##__VA_ARGS__)
+#define SSULogVerbose(frmt, ...)    DDLogVerbose(frmt, ##__VA_ARGS__)
+#endif
+
 
 @end
