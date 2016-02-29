zeshadow/ssumobile-ios

commit 43c86e33f35d4bb62b3c26e3a737b939ff711ab2

Update calendar to non-delta based API

SSUMobile/Modules/Calendar/Model/SSUCalendarBuilder.m
@@ -46,17 +46,28 @@
     dateFormatter.locale = [NSLocale currentLocale];
     dateFormatter.timeZone = [NSTimeZone systemTimeZone];
     
+    // Calendar is no longer date delta-ed - instead, all events which have a start date
+    // at most one month in the past and up until the indefinite future are sent.
+    if (events.count > 0) {
+        NSArray * existingEvents = [SSUMoonlightBuilder allObjectsWithEntityName:SSUCalendarEntityEvent
+                                                                         context:self.context];
+        SSULogDebug(@"Deleting %lu old events", (unsigned long)existingEvents.count);
+        for (SSUEvent * event in existingEvents) {
+            [event.managedObjectContext deleteObject:event];
+        }
+    }
+    
     for (NSDictionary * eventData in events) {
         SSUMoonlightDataMode mode = [self modeFromJSONData:eventData];
         NSNumber * eventId = @([eventData[SSUCalendarEventKeyID] integerValue]);
         SSUEvent * event = [SSUCalendarBuilder eventWithID:eventId inContext:self.context];
-        if (event == nil) {
-            continue;
-        }
-        if (mode == SSUMoonlightDataModeDeleted) {
-            [self.context deleteObject:event];
-            continue;
-        }
+//        if (event == nil) {
+//            continue;
+//        }
+//        if (mode == SSUMoonlightDataModeDeleted) {
+//            [self.context deleteObject:event];
+//            continue;
+//        }
         
         event.title = eventData[SSUCalendarEventKeyTitle];
         event.startDate = [dateFormatter dateFromString:eventData[SSUCalendarEventKeyStart]];

SSUMobile/Modules/Calendar/SSUCalendarModule.m
@@ -64,9 +64,9 @@
 
 - (void) updateData:(void (^)())completion {
     SSULogDebug(@"Update Calendar NEW");
-    NSString * lastUpdate = [[NSUserDefaults standardUserDefaults] objectForKey:SSUUserDefaultsCalendarUpdatedDate];
-    NSDate * date = (lastUpdate != nil) ? [self.dateFormatter dateFromString:lastUpdate] : nil;
-    [SSUMoonlightCommunicator getJSONFromPath:@"calendar" sinceDate:date completion:^(id json, NSError *error) {
+//    NSString * lastUpdate = [[NSUserDefaults standardUserDefaults] objectForKey:SSUUserDefaultsCalendarUpdatedDate];
+//    NSDate * date = (lastUpdate != nil) ? [self.dateFormatter dateFromString:lastUpdate] : nil;
+    [SSUMoonlightCommunicator getJSONFromPath:@"calendar" sinceDate:nil completion:^(id json, NSError *error) {
         if (error != nil) {
             SSULogError(@"Error while attemping to update Calendar: %@", error);
             if (completion) {

commit 6047ada0a538698b0ef3a230fe643c12ced225e2

Fix issue with deleted events introduced by 43c86e3

SSUMobile/Modules/Calendar/Model/SSUCalendarBuilder.m
@@ -46,28 +46,11 @@
     dateFormatter.locale = [NSLocale currentLocale];
     dateFormatter.timeZone = [NSTimeZone systemTimeZone];
     
-    // Calendar is no longer date delta-ed - instead, all events which have a start date
-    // at most one month in the past and up until the indefinite future are sent.
-    if (events.count > 0) {
-        NSArray * existingEvents = [SSUMoonlightBuilder allObjectsWithEntityName:SSUCalendarEntityEvent
-                                                                         context:self.context];
-        SSULogDebug(@"Deleting %lu old events", (unsigned long)existingEvents.count);
-        for (SSUEvent * event in existingEvents) {
-            [event.managedObjectContext deleteObject:event];
-        }
-    }
+    NSMutableArray * ids = [NSMutableArray new];
     
     for (NSDictionary * eventData in events) {
-        SSUMoonlightDataMode mode = [self modeFromJSONData:eventData];
         NSNumber * eventId = @([eventData[SSUCalendarEventKeyID] integerValue]);
         SSUEvent * event = [SSUCalendarBuilder eventWithID:eventId inContext:self.context];
-//        if (event == nil) {
-//            continue;
-//        }
-//        if (mode == SSUMoonlightDataModeDeleted) {
-//            [self.context deleteObject:event];
-//            continue;
-//        }
         
         event.title = eventData[SSUCalendarEventKeyTitle];
         event.startDate = [dateFormatter dateFromString:eventData[SSUCalendarEventKeyStart]];
@@ -78,7 +61,13 @@
         event.organization = eventData[SSUCalendarEventKeyOrganization];
         event.category = eventData[SSUCalendarEventKeyCategory];
         
+        [ids addObject:[event id]];
     }
+    
+    // Calendar is no longer date delta-ed - instead, all events which have a start date
+    // at most one month in the past and up until the indefinite future are sent.
+    NSPredicate * deletePredicate = [NSPredicate predicateWithFormat:@"NOT (id  IN %@)", ids];
+    [SSUMoonlightBuilder deleteObjectsWithEntityName:SSUCalendarEntityEvent matchingPredicate:deletePredicate context:self.context];
     SSULogDebug(@"Finished Events: %f",[[NSDate date] timeIntervalSinceDate:buildStart]);
 }
 