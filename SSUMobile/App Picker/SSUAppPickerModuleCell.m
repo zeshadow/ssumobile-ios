f1e6a7e0d74e76f653f6f2384bb491b8b3e5a429
Eric Amorde <eric.amorde@gmail.com>
2016-08-31 18:41:20 -0700

    Fix autolayout issue with iOS 10, move layout logic to cell class

--- SSUMobile/App Picker/SSUAppPickerModuleCell.m
+++ SSUMobile/App Picker/SSUAppPickerModuleCell.m
@@ -22,12 +22,35 @@
     if (_module == module) return;
     
     _module = module;
-    [self.button setImage:[_module imageForHomeScreen] forState:UIControlStateNormal];
+    
+    [self updateDisplay];
+}
+
+- (void) updateDisplay {
+    [self.button setImage:[self.module imageForHomeScreen] forState:UIControlStateNormal];
     [self.button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     self.button.contentMode = UIViewContentModeScaleAspectFit;
     self.button.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    self.button.accessibilityLabel = [_module title];
-    self.moduleLabel.text = [_module title];
+    self.button.accessibilityLabel = self.module.title;
+    self.moduleLabel.text = self.module.title;
+    
+    const CGFloat offset = 8.0;
+    
+    [self.button mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.button.superview);
+        make.left.equalTo(self.button.superview).offset(offset);
+        make.right.equalTo(self.button.superview).offset(-offset);
+        make.bottom.equalTo(self.moduleLabel.mas_top).offset(-offset);
+    }];
+    
+    [self.moduleLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.button.mas_bottom);
+        make.left.equalTo(self.moduleLabel.superview).offset(offset);
+        make.right.equalTo(self.moduleLabel.superview).offset(offset);
+        make.bottom.equalTo(self.moduleLabel.superview).offset(-offset/2.0);
+    }];
+    
+    [self.moduleLabel setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
 }
 
 - (void) buttonPressed:(UIButton *)button {
