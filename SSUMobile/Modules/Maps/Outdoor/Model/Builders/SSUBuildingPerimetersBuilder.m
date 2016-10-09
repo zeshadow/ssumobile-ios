a4c52d3d1ac4298b90943cf23db7183bcfed97a3
Eric Amorde <eric.amorde@gmail.com>
2016-10-09 00:33:51 -0700

    Fix building perimeters being ordered incorrectly (#14)

--- SSUMobile/Modules/Maps/Outdoor/Model/Builders/SSUBuildingPerimetersBuilder.m
+++ SSUMobile/Modules/Maps/Outdoor/Model/Builders/SSUBuildingPerimetersBuilder.m
@@ -13,42 +13,57 @@
 
 NSString * const SSUPerimeterPointIdKey = @"point";
 NSString * const SSUPerimeterBuildingIdKey = @"building";
+NSString * const SSUPerimeterIndexKey = @"index";
 
 @implementation SSUBuildingPerimetersBuilder
 
 - (void) build:(NSArray *)perimeters {
     SSULogDebug(@"Building Perimeters: %lu", (unsigned long)perimeters.count);
+    if (perimeters.count == 0) {
+        SSULogDebug(@"Finished building perimeters");
+        return;
+    }
     NSManagedObjectContext* directoryContext = [[SSUDirectoryModule sharedInstance] backgroundContext];
     
     NSMutableDictionary <NSString *, NSMutableArray *> * perimetersByBuilding = [NSMutableDictionary new];
     
-    NSMutableArray * ids = [NSMutableArray new];
     for (NSDictionary * perimeterData in perimeters) {
-        [ids addObject:perimeterData[SSUMoonlightManagerKeyID]];
-        NSString * buildingId = SSUMoonlightBuilderStringify(perimeterData[SSUMoonlightManagerKeyID]);
+        NSString * buildingId = SSUMoonlightBuilderStringify(perimeterData[SSUPerimeterBuildingIdKey]);
         if (perimetersByBuilding[buildingId] == nil) {
             perimetersByBuilding[buildingId] = [NSMutableArray new];
         }
         [perimetersByBuilding[buildingId] addObject:perimeterData];
     }
+    // Now sort the perimeters by their respective indices
+    for (NSString* buildingID in [perimetersByBuilding allKeys]) {
+        NSArray * perimeters = perimetersByBuilding[buildingID];
+        perimeters = [perimeters sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            return [obj1[SSUPerimeterIndexKey] compare:obj2[SSUPerimeterIndexKey]];
+        }];
+        perimetersByBuilding[buildingID] = [perimeters mutableCopy];
+    }
     
     for (NSString* buildingID in perimetersByBuilding) {
-        [directoryContext performBlock:^{
+        [directoryContext performBlockAndWait:^{
             SSUBuilding* building = [SSUDirectoryBuilder buildingWithID:buildingID inContext:directoryContext];
             NSString * buildingName = building.displayName;
             [self.context performBlockAndWait:^{
                 SSUMapBuildingPerimeter* perimeter = [self perimeterForBuilding:building];
                 [perimeter removeLocations:perimeter.locations];
+                NSMutableOrderedSet * locations = [NSMutableOrderedSet new];
                 for (NSDictionary* pointData in perimetersByBuilding[buildingID]) {
                     NSNumber* pID = pointData[SSUPerimeterPointIdKey];
                     SSUMapPoint* point = [SSUBuildingPerimetersBuilder mapPointWithID:pID.stringValue inContext:self.context];
-                    [perimeter addLocationsObject:point];
+                    [locations addObject:point];
                 }
+                [perimeter setLocations:locations];
                 perimeter.buildingName = buildingName;
             }];
         }];
     }
-    NSPredicate * deletePredicate = [NSPredicate predicateWithFormat:@"NOT (id  IN %@)", ids];
+    
+    // If a building has been deleted, we need to delete the perimeters for it
+    NSPredicate * deletePredicate = [NSPredicate predicateWithFormat:@"NOT (buildingID IN %@)", perimetersByBuilding.allKeys];
     [SSUMoonlightBuilder deleteObjectsWithEntityName:SSUOutdoorMapEntityBuildingPerimeter matchingPredicate:deletePredicate context:self.context];
     SSULogDebug(@"Finished building perimeters");
     [self saveContext];
